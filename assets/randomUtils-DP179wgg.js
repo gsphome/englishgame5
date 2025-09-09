035147a8fdaa1b9ee07945105eef2c35811e60cc
Genil Suarez <genil.suarez@gmail.com>
2025-09-09 18:30:11 -0300

    Updates

---
+++ assets/randomUtils-DP179wgg.js
@@ -0,0 +1,1 @@
+function n(f){const t=[...f];for(let o=t.length-1;o>0;o--){const r=Math.floor(Math.random()*(o+1));[t[o],t[r]]=[t[r],t[o]]}return t}export{n as s};
---
+++ assets/useLearningCleanup-k8WkhSTw.js
@@ -0,0 +1,1 @@
+import{r as t,t as a}from"./index-D6yE4j4L.js";const s=()=>(t.useEffect(()=>()=>{a.clearOnNavigation()},[]),{clearGameToasts:()=>{a.clearOnNavigation()}});export{s as u};
