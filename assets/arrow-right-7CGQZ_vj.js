940813204e4b568d94652d45cc07fc9546d33298
Genil Suarez <genil.suarez@gmail.com>
2025-09-09 21:12:00 -0300

    Updates

--- assets/arrow-right-7CGQZ_vj.js
+++ assets/arrow-right-7CGQZ_vj.js
@@ -1,4 +1,4 @@
-import{c as r}from"./index-Tghuj-1g.js";/**
+import{c as r}from"./index-BKxTNVQ3.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/useLearningCleanup-qXJUIooo.js
+++
@@ -1,1 +0,0 @@
-import{r as t,t as a}from"./index-Tghuj-1g.js";const s=()=>(t.useEffect(()=>()=>{a.clearOnNavigation()},[]),{clearGameToasts:()=>{a.clearOnNavigation()}});export{s as u};
